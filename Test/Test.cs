3b357a83d421eed58321b4c931b0b3b1bd022bcf
Arthur <94467283+avlrd@users.noreply.github.com>
2024-03-18 14:16:57 +0000

    Calcul mensualité cas simple

--- Test/Test.cs
+++ Test/Test.cs
@@ -16,3 +16,12 @@ public class Test
 		Assert.Equal(484.30, mensualité);
 	}
 }
+
+public abstract class CredImmo
+{
+	public static double CalculMensualité(ushort montant, ushort duree, double taux)
+	{
+		double mensualite = Math.Round(montant*taux/100/12/(1-Math.Pow(1+taux/100/12, -duree)), 2);
+		return mensualite;
+	}
+}
