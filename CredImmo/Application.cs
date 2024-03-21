8e4c60b577ce91010522cb3da1258c7740ff1f4e
Arthur <94467283+avlrd@users.noreply.github.com>
2024-03-21 12:18:27 +0000

    Application logic

--- CredImmo/Application.cs
+++ CredImmo/Application.cs
@@ -6,6 +6,24 @@ public class Application
 	{
 		if(!IArgsValidation.ValidationArguments(args))
 			return;
-		Console.WriteLine("Hello :)");
-	}
+			
+		uint montant = uint.Parse(args[0]);
+		ushort duree = ushort.Parse(args[1]);
+		double taux = double.Parse(args[2]);
+		string cheminDestination = args[3];
+		
+		double mensualite = ICalculateur.CalculMensualite(montant, duree, taux);
+		double total = ICalculateur.CalculTotal(montant, duree, taux);
+
+        using StreamWriter stream = new(cheminDestination);
+
+        stream.WriteLine(ICSV.Total(total));
+        stream.WriteLine(ICSV.Labels());
+        for (int i = 1; i <= duree; i++)
+        {
+            double capitalRembourse = mensualite * i;
+            double restantDu = total - capitalRembourse;
+            stream.WriteLine(ICSV.Mensualite(i, capitalRembourse, restantDu));
+        }
+    }
 }
