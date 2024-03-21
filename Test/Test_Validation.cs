cc8981581ae5f09eb0b30b72bfdb76aceb33d3ea
Arthur <94467283+avlrd@users.noreply.github.com>
2024-03-21 09:27:09 +0000

    Tests modifiés car finalement on ne devrait pas tester les valeurs à ce moment là du programme

--- Test/Test_Validation.cs
+++ Test/Test_Validation.cs
@@ -11,44 +11,14 @@ public class Test_Validation
 	}
 
 	[Theory]
-	[InlineData("10000", "108", "1.0", ".")]
-	[InlineData("50000", "301", "0.01", ".")]
-	[InlineData("50000", "107", "1.0", ".")]
-	[InlineData("50000", "108", "-1.0", ".")]
+	[InlineData("bonjour", "108", "1.0", ".")]
+	[InlineData("50000", "bonjour", "1.0", ".")]
+	[InlineData("50000", "108", "bonjour", ".")]
+	[InlineData("", "108", "1.0", ".")]
+	[InlineData("Bonjour 50000", "108", "1.0", ".")]
+	[InlineData("25000+25000", "108", "1.0", ".")]
 	public void Test_VerificationArguments_Fail(string arg1, string arg2, string arg3, string arg4)
 	{
 		Assert.False(IArgsValidation.ValidationArguments([arg1, arg2, arg3, arg4]));
-	}
-}
-
-public interface IArgsValidation
-{
-	public static bool ValidationArguments(string[] args)
-	{
-		if(args.Length != 4)
-		{
-			Console.WriteLine("Utilisation : CredImmo <montant> <duree> <taux> <destination fichier csv>");
-			return false;
-		}
-
-		if(!uint.TryParse(args[0], out uint arg1) || arg1 < 50000)
-		{
-			Console.WriteLine($"Erreur : Le montant doit être un nombre entier supérieur ou égal à 50000. (Obtenu : {arg1})");
-			return false;
-		}
-
-		if(!ushort.TryParse(args[1], out ushort arg2) || arg2 < 108 || arg2 > 300)
-		{
-			Console.WriteLine($"Erreur : La durée doit être un nombre entier compris entre 108 et 300. (Obtenu : {arg2})");
-			return false;
-		}
-
-		if(!double.TryParse(args[2], out double arg3) || arg3 < 0.0)
-		{
-			Console.WriteLine($"Erreur : Le taux doit être un nombre décimal supérieur ou égal à 0. (Obtenu : {arg3})");
-			return false;
-		}
-
-		return true;
 	}
 }
