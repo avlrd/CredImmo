60532bfc964e5b6879440b0221c6cb984de10b03
Arthur <94467283+avlrd@users.noreply.github.com>
2024-03-21 09:47:24 +0000

    Test calcul mensualité taux supérieur à 100 et max values

--- Test/Calculateur/Test_CalculMensualite.cs
+++ Test/Calculateur/Test_CalculMensualite.cs
@@ -5,7 +5,7 @@ public class Test_CalculMensualite
 	[Fact]
 	public void CalculMensualite_CasSimpleValide()
 	{
-		ushort montant = 50000;
+		uint montant = 50000;
 		ushort duree = 108;
 		double taux = 1;
 
@@ -14,12 +14,25 @@ public class Test_CalculMensualite
 		Assert.Equal(484.30, mensualité);
 	}
 
+	[Fact]
+	public void CalculMensualite_MaxValues()
+	{
+		uint montant = uint.MaxValue;
+		ushort duree = 300;
+		double taux = 100;
+
+		double mensualité = ICalculateur.CalculMensualite(montant, duree, taux);
+
+		Assert.Equal(357913941.25999999, mensualité);
+	}
+
 	[Theory]
 	[InlineData(49999, 108, 1)]
 	[InlineData(50000, 107, 1)]
 	[InlineData(50000, 301, 1)]
 	[InlineData(50000, 108, -1)]
-	public void CalculMensualite_InvalidArgs(ushort montant, ushort duree, double taux)
+	[InlineData(50000, 108, 100.01)]
+	public void CalculMensualite_InvalidArgs(uint montant, ushort duree, double taux)
 	{
 		Assert.Throws<ArgumentException>(() => ICalculateur.CalculMensualite(montant, duree, taux));
 	}
